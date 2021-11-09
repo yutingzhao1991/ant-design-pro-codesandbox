946f63fdc630fa3c450daa203720ee426db7321b
愚道 <tingzhao.ytz@antfin.com>
2021-11-09 19:09:57 +0800

    fix: fix rule mock data

--- mock/rule.js
+++ mock/rule.js
@@ -1,6 +1,3 @@
-// eslint-disable-next-line import/no-extraneous-dependencies
-import { parse } from 'url';
-
 // mock tableListDataSource
 const genList = (current, pageSize) => {
   const tableListDataSource = [];
@@ -31,59 +28,14 @@ const genList = (current, pageSize) => {
 
 let tableListDataSource = genList(1, 100);
 
-function getRule(req, res, u) {
-  let realUrl = u;
-  if (
-    !realUrl ||
-    Object.prototype.toString.call(realUrl) !== '[object String]'
-  ) {
-    realUrl = req.url;
-  }
+function getRule(req, res) {
+  const params = req.query;
   const { current = 1, pageSize = 10 } = req.query;
-  const params = parse(realUrl, true);
 
   let dataSource = [...tableListDataSource].slice(
     (current - 1) * pageSize,
     current * pageSize,
   );
-  if (params.sorter) {
-    const sorter = JSON.parse(params.sorter);
-    dataSource = dataSource.sort((prev, next) => {
-      let sortNumber = 0;
-      Object.keys(sorter).forEach((key) => {
-        if (sorter[key] === 'descend') {
-          if (prev[key] - next[key] > 0) {
-            sortNumber += -1;
-          } else {
-            sortNumber += 1;
-          }
-          return;
-        }
-        if (prev[key] - next[key] > 0) {
-          sortNumber += 1;
-        } else {
-          sortNumber += -1;
-        }
-      });
-      return sortNumber;
-    });
-  }
-  if (params.filter) {
-    const filter = JSON.parse(params.filter);
-    if (Object.keys(filter).length > 0) {
-      dataSource = dataSource.filter((item) => {
-        return Object.keys(filter).some((key) => {
-          if (!filter[key]) {
-            return true;
-          }
-          if (filter[key].includes(`${item[key]}`)) {
-            return true;
-          }
-          return false;
-        });
-      });
-    }
-  }
 
   if (params.name) {
     dataSource = dataSource.filter((data) =>
@@ -107,78 +59,6 @@ function getRule(req, res, u) {
   return res.json(result);
 }
 
-function postRule(req, res, u, b) {
-  let realUrl = u;
-  if (
-    !realUrl ||
-    Object.prototype.toString.call(realUrl) !== '[object String]'
-  ) {
-    realUrl = req.url;
-  }
-
-  const body = (b && b.body) || req.body;
-  const { name, desc, key } = body;
-
-  switch (req.method) {
-    /* eslint no-case-declarations:0 */
-    case 'DELETE':
-      tableListDataSource = tableListDataSource.filter(
-        (item) => key.indexOf(item.key) === -1,
-      );
-      break;
-    case 'POST':
-      (() => {
-        const i = Math.ceil(Math.random() * 10000);
-        const newRule = {
-          key: tableListDataSource.length,
-          href: 'https://ant.design',
-          avatar: [
-            'https://gw.alipayobjects.com/zos/rmsportal/eeHMaZBwmTvLdIwMfBpg.png',
-            'https://gw.alipayobjects.com/zos/rmsportal/udxAbMEhpwthVVcjLXik.png',
-          ][i % 2],
-          name,
-          owner: '曲丽丽',
-          desc,
-          callNo: Math.floor(Math.random() * 1000),
-          status: (Math.floor(Math.random() * 10) % 2).toString(),
-          updatedAt: new Date(),
-          createdAt: new Date(),
-          progress: Math.ceil(Math.random() * 100),
-        };
-        tableListDataSource.unshift(newRule);
-        return res.json(newRule);
-      })();
-      return;
-
-    case 'PUT':
-      (() => {
-        let newRule = {};
-        tableListDataSource = tableListDataSource.map((item) => {
-          if (item.key === key) {
-            newRule = { ...item, desc, name };
-            return { ...item, desc, name };
-          }
-          return item;
-        });
-        return res.json(newRule);
-      })();
-      return;
-    default:
-      break;
-  }
-
-  const result = {
-    list: tableListDataSource,
-    pagination: {
-      total: tableListDataSource.length,
-    },
-  };
-
-  res.json(result);
-}
-
 export default {
   'GET /api/rule': getRule,
-  'POST /api/rule': postRule,
-  'DELETE /api/rule': postRule,
 };
