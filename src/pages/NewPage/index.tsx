489bb3caf9517cdcb2ed70a53c4cde6fc947d152
愚道 <tingzhao.ytz@antfin.com>
2021-11-09 17:36:38 +0800

    feat: add pro-table

--- src/pages/NewPage/index.tsx
+++ src/pages/NewPage/index.tsx
@@ -1,14 +1,53 @@
 import React from 'react';
 import { PageContainer } from '@ant-design/pro-layout';
+import ProTable from '@ant-design/pro-table';
+import type { ProColumns } from '@ant-design/pro-table';
+
+interface TableListItem {
+  key: number;
+  name: string;
+  desc: string;
+}
 
 const NewPage: React.FC = () => {
+  const columns: ProColumns<TableListItem>[] = [
+    {
+      title: '规则名称',
+      dataIndex: 'name',
+    },
+    {
+      title: '规则描述',
+      dataIndex: 'desc',
+    },
+  ];
+
   return (
     <PageContainer
       header={{
         title: '新的页面',
       }}
     >
-      Hello World!
+      <ProTable<TableListItem>
+        columns={columns}
+        request={async () => {
+          return {
+            data: [
+              {
+                key: 1,
+                name: '规则一',
+                desc: '规则一的描述',
+              },
+              {
+                key: 2,
+                name: '规则二',
+                desc: '规则二的描述',
+              },
+            ],
+            success: true,
+            total: 100,
+          };
+        }}
+      />
     </PageContainer>
   );
 };
