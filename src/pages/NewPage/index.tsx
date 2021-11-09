f992ce846ac3774f25387faea8600786570c1932
愚道 <tingzhao.ytz@antfin.com>
2021-11-09 17:56:51 +0800

    feat: add mock and service

--- src/pages/NewPage/index.tsx
+++ src/pages/NewPage/index.tsx
@@ -3,11 +3,8 @@ import { PageContainer } from '@ant-design/pro-layout';
 import ProTable from '@ant-design/pro-table';
 import type { ProColumns } from '@ant-design/pro-table';
 
-interface TableListItem {
-  key: number;
-  name: string;
-  desc: string;
-}
+import type { TableListItem } from './service';
+import { rule } from './service';
 
 const NewPage: React.FC = () => {
   const columns: ProColumns<TableListItem>[] = [
@@ -27,27 +24,7 @@ const NewPage: React.FC = () => {
         title: '新的页面',
       }}
     >
-      <ProTable<TableListItem>
-        columns={columns}
-        request={async () => {
-          return {
-            data: [
-              {
-                key: 1,
-                name: '规则一',
-                desc: '规则一的描述',
-              },
-              {
-                key: 2,
-                name: '规则二',
-                desc: '规则二的描述',
-              },
-            ],
-            success: true,
-            total: 100,
-          };
-        }}
-      />
+      <ProTable<TableListItem> columns={columns} request={rule} />
     </PageContainer>
   );
 };
